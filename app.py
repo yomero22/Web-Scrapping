91d5e49451b8ad2e364d1541667e3ab36e093c2a
yomero22 <46915889+yomero22@users.noreply.github.com>
2019-05-27 23:29:41 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = PyMongo(app)
+
+@app.route("/")
+def.index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+#scrape data and pull into Mongo DB
+@app.route('/scrape')
+    def get():
+        mars = mongo.db.mars
+        marsdata = scrape_mars.scrape()
+        mars.update({}, marsdata, upsert=True)
+        return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run()
